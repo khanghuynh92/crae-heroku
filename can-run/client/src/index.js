f4d08c97d1c30896cb73834e9810219f751a0c22
Administrator <khanghuynh92@gmail.com>
2018-04-13 15:22:54 +0700

    fix 2

--- can-run/client/src/index.js
+++ can-run/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './components/Calculator';
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
